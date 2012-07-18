ce12ab7e701a0e293e708693caf45bdff3067469
Roland Arsenault <roland@ccom.unh.edu>
2012-07-18 17:08:24 -0400

    Can now graph all contents of the gedcom or just the ancestors of the oprional name passed as a second parameter.

--- graph_gedcom.py
+++ graph_gedcom.py
@@ -3,29 +3,68 @@
 import gedcom
 import sys
 
+if len(sys.argv) not in (2,3):
+    print 'usage: graph_gedcom.py in.ged [name]'
+    exit(1)
+    
+
 g = gedcom.Gedcom(sys.argv[1])
 llg = gedcom.LineageLinkedGedcom(g)
 
-print 'digraph g {'
-for k,v in llg.index.iteritems():
-    print '\t'+k[1:-1],
-    if k[1] == 'I':
-        print '[label="'+str(v.records['NAME'][0])+'", ',
-        if v.getValue('SEX') == 'M':
+
+root = None
+
+if len(sys.argv) == 3:
+    root_name = sys.argv[2]
+    for i in llg.individuals:
+        if i.isName(root_name):
+            root = i
+            break
+        
+
+def print_record(r):
+    print '\t'+r.xref_id[1:-1],
+    if r.xref_id[1] == 'I':
+        print '[label="'+str(r.records['NAME'][0])+'", ',
+        if r.getValue('SEX') == 'M':
             print 'color=Blue, shape=box]'
         else:
             print 'color=Pink, shape=box]'
-        f = v.getValue('FAMC')
+        f = r.getValue('FAMC')
         if f is not None:
-            print '\t'+k[1:-1],'->',f.xref_id[1:-1]
+            print '\t'+f.xref_id[1:-1],'->',r.xref_id[1:-1]
     else:
         print
-        if v.getValue('HUSB') is not None:
-            print k[1:-1],'->',v.getValue('HUSB').xref_id[1:-1]
-        if v.getValue('WIFE') is not None:
-            print k[1:-1],'->',v.getValue('WIFE').xref_id[1:-1]
+        if r.getValue('HUSB') is not None:
+            print '\t'+r.getValue('HUSB').xref_id[1:-1],'->',r.xref_id[1:-1]
+        if r.getValue('WIFE') is not None:
+            print '\t'+r.getValue('WIFE').xref_id[1:-1],'->',r.xref_id[1:-1]
+    
+        
+print 'digraph g {'
+
+if root is None:
+    for k,v in llg.index.iteritems():
+        print_record(v)
+else:
+    q = [root]
+    seen = []
+    while len(q):
+        if not q[0] in seen:
+            print_record(q[0])
+            f = q[0].getValue('FAMC')
+            if f is not None:
+                if not f in seen:
+                    q.append(f)
+                    if f.getValue('HUSB') is not None:
+                        if not f.getValue('HUSB') in seen:
+                            q.append(f.getValue('HUSB'))
+                    if f.getValue('WIFE') is not None:
+                        if not f.getValue('WIFE') in seen:
+                            q.append(f.getValue('WIFE'))
+            seen.append(q[0])
+        q = q[1:]
         
 print '}'
 
 
-	
