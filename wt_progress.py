a050e9c758db7a86b2427960ede9b377eca433f2
Roland Arsenault <roland@ccom.unh.edu>
2012-07-18 17:47:16 -0400

    Adding tool to calculate how much of your tree is done

---
+++ wt_progress.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import gedcom
+import sys
+
+if len(sys.argv) != 3:
+    print 'usage: wt_progress.py in.ged name'
+    exit(1)
+    
+
+g = gedcom.Gedcom(sys.argv[1])
+llg = gedcom.LineageLinkedGedcom(g)
+
+
+root = None
+
+root_name = sys.argv[2]
+for i in llg.individuals:
+    if i.isName(root_name):
+        root = i
+        break
+        
+if root is None:
+    print 'individual not found:',root_name
+    exit(1)
+
+generations = [[root]]
+cur_gen = 0
+
+while cur_gen < len(generations):
+    for i in generations[cur_gen]:
+        f = i.getValue('FAMC')
+        if f is not None:
+            if len(generations) <= cur_gen+1:
+                generations.append([])
+            if f.getValue('HUSB') is not None:
+                generations[cur_gen+1].append(f.getValue('HUSB'))
+            if f.getValue('WIFE') is not None:
+                generations[cur_gen+1].append(f.getValue('WIFE'))
+    cur_gen += 1
+
+for i in range (len(generations)):
+    print 'generation',i,'-',len(generations[i]),'of',2**i,'(',len(generations[i])*100/float(2**i),'%)'
