2259ed15355f5d8c220a37af768c363fe2b48e4f
Roland Arsenault <roland@rolker.net>
2013-10-25 02:28:56 -0400

    Initial components of a WikiTree Javascipt toolkit.

---
+++ web/wtPerson.js
@@ -0,0 +1,42 @@
+// Copyright Roland Arsenault
+
+;(function(wt, $, undefined) {
+
+  wt.Person = function(user_id){
+    this.user_id = user_id;
+    this.loaded = false;
+  }
+
+  wt.Person.prototype.load = function(callback){
+    if(!this.loaded && !this.loading){
+      this.loading = true;
+      var self = this;
+      $.post('/api.php', 
+        { 'action': 'getPerson', 'key': this.user_id, 'fields': 'Name,FirstName,LastNameCurrent,Gender,Father,Mother', 'format': 'json' }
+      ).done(function(data) { 
+        if (data[0].status) { 
+          console.log("There was an error getting the person:",data);
+          self.loading = false;
+          if(callback != undefined)
+            callback(self);
+        } else {
+          for(var attrib in data[0].person)
+            self[attrib] = data[0].person[attrib];
+          $.post('/api.php', {
+            'action': 'getPerson', 'key': self.user_id, 'fields': 'Id,Siblings,Spouses,Children', 'format': 'json' 
+            }).done(function(data) { 
+              for(var attrib in data[0].person)
+                self[attrib] = data[0].person[attrib];
+              self.loading = false;
+              self.loaded = true;
+              if(callback != undefined)
+                callback(self);
+            });
+
+        }
+      });
+    } else if(callback != undefined)
+      callback(self);
+  }
+  
+})(window.wt = window.wt || {}, jQuery);
