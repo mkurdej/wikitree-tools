50e38dfee6e7773dcbfc8a35f826dc5584c64cfa
Roland Arsenault <roland@rolker.net>
2013-12-04 02:17:42 -0500

    Visited profiles are now remembered, so checks regarding parent's age at birth can be made if parent was previously loaded.

--- chrome/wt-tools/popup.js
+++ chrome/wt-tools/popup.js
@@ -1,18 +1,6 @@
-
-
-
-
-$(document).ready( function(){
-	$("#showToolsButton").click(function(){
-		chrome.tabs.query({active: true, currentWindow: true},function(tabs){
-			chrome.tabs.sendMessage(tabs[0].id,"showStatus");
-			window.close();
-		});
-	});
 	
-	chrome.tabs.query({active: true, currentWindow: true},function(tabs){
-		chrome.tabs.sendMessage(tabs[0].id,"showStatus");
-		window.close();
-	});
-
+chrome.tabs.query({active: true, currentWindow: true},function(tabs){
+	chrome.tabs.sendMessage(tabs[0].id,"showStatus");
+	window.close();
 });
+
