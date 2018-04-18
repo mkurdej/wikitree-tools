aed8eaabc24e771c7c77aee60631f470efc6564d
Marek Kurdej <marek@quasardb.net>
2018-04-18 08:43:27 +0200

    Format.

--- wt_lint.py
+++ wt_lint.py
@@ -3,8 +3,10 @@
 import gedcom
 import datetime
 
+
 def stdPrint(data=''):
     print data
+
 
 def lint(fname, options, out=stdPrint):
 
@@ -14,44 +16,44 @@ def lint(fname, options, out=stdPrint):
     for o in options:
         out(o)
         if '=' in o:
-            k,v = o.split('=',1)
+            k, v = o.split('=', 1)
             opts[k] = v
         else:
             opts[o] = None
 
-    if opts.has_key('output'):            
-        output = open(opts['output'],'w')
+    if opts.has_key('output'):
+        output = open(opts['output'], 'w')
 
     maxage = None
     if opts.has_key('maxage'):
         maxage = float(opts['maxage'])
-                
-                
-        
+
     g = gedcom.Gedcom(fname)
     llg = gedcom.LineageLinkedGedcom(g)
 
     if output is not None:
         output.write('<html><body>\n')
-        output.write('<h1><a href="http://www.wikitree.com">WikiTree</a> Lint</h1>\n')
-        output.write('<p>Report run at: '+str(datetime.datetime.now())+'</p>\n')
-        output.write('<p>input: '+fname+'</p>\n')
+        output.write(
+            '<h1><a href="http://www.wikitree.com">WikiTree</a> Lint</h1>\n')
+        output.write('<p>Report run at: ' +
+                     str(datetime.datetime.now()) + '</p>\n')
+        output.write('<p>input: ' + fname + '</p>\n')
         output.write('<el>Lint options\n')
-        for k,v in opts.iteritems():
-            output.write('<li>'+k)
+        for k, v in opts.iteritems():
+            output.write('<li>' + k)
             if v is not None:
-                output.write(': '+v)
+                output.write(': ' + v)
             output.write('</li>\n')
         output.write('</el>\n')
 
-
     for i in llg.individuals:
-            
+
         if i.records.has_key('WWW'):
             problems = []
             if maxage is not None and i.getAge() is not None and i.getAge() > maxage:
-                problems.append('age {0} greater than maximum age {1}'.format(i.getAge(),maxage))
-                
+                problems.append(
+                    'age {0} greater than maximum age {1}'.format(i.getAge(), maxage))
+
             if opts.has_key('birth') and i.get('BIRT').get('DATE') is None:
                 problems.append('missing birth date')
             if opts.has_key('death') and i.get('DEAT') is None:
@@ -66,11 +68,11 @@ def lint(fname, options, out=stdPrint):
                 problems.append('todo found in bio')
             if len(problems):
                 if output is not None:
-                    output.write(i.html()+'\n')
+                    output.write(i.html() + '\n')
                     output.write('<h3>Problems:</h3>\n')
                     output.write('<e1>\n')
                     for p in problems:
-                        output.write('<li>'+p+'</li>\n')
+                        output.write('<li>' + p + '</li>\n')
                     output.write('</e1>\n')
                 out('')
                 out('\n'.join(problems))
@@ -92,5 +94,4 @@ if __name__ == '__main__':
         print '\t\toutput=report.html'
         exit(1)
 
-    lint(sys.argv[1],sys.argv[2:])
-    
+    lint(sys.argv[1], sys.argv[2:])
