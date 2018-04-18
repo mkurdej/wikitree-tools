a4dad9f1a1330b1d34fd8b37ceda14d330562c00
Marek Kurdej <marek@quasardb.net>
2018-04-18 08:56:48 +0200

    Refactor check_individual.

--- wt_lint.py
+++ wt_lint.py
@@ -23,21 +23,60 @@ def parse_options(options, out):
     return opts
 
 
-def write_report(filename, opts, output):
-    if output is not None:
-        output.write('<html><body>\n')
-        output.write(
-            '<h1><a href="http://www.wikitree.com">WikiTree</a> Lint</h1>\n')
-        output.write('<p>Report run at: ' +
-                     str(datetime.datetime.now()) + '</p>\n')
-        output.write('<p>input: ' + filename + '</p>\n')
-        output.write('<el>Lint options\n')
-        for key, value in opts.iteritems():
-            output.write('<li>' + key)
-            if value is not None:
-                output.write(': ' + value)
-            output.write('</li>\n')
-        output.write('</el>\n')
+def write_report_header(filename, opts, output):
+    if output is None:
+        return
+
+    output.write('<html><body>\n')
+    output.write(
+        '<h1><a href="http://www.wikitree.com">WikiTree</a> Lint</h1>\n')
+    output.write('<p>Report run at: ' +
+                 str(datetime.datetime.now()) + '</p>\n')
+    output.write('<p>input: ' + filename + '</p>\n')
+    output.write('<el>Lint options\n')
+    for key, value in opts.iteritems():
+        output.write('<li>' + key)
+        if value is not None:
+            output.write(': ' + value)
+        output.write('</li>\n')
+    output.write('</el>\n')
+
+
+def check_individual(individual, opts, maxage, output, out):
+    if not individual.records.has_key('WWW'):
+        return
+
+    problems = []
+    if maxage is not None and individual.getAge() is not None and individual.getAge() > maxage:
+        problems.append(
+            'age {0} greater than maximum age {1}'.format(individual.getAge(), maxage))
+
+    if opts.has_key('birth') and individual.get('BIRT').get('DATE') is None:
+        problems.append('missing birth date')
+    if opts.has_key('death') and individual.get('DEAT') is None:
+        problems.append('missing date of death')
+    if individual.get('SEX') is None:
+        problems.append('sex not defined')
+    for obj in individual.getAll('OBJE'):
+        if obj.get('FORM').value == 'Message':
+            if obj.get('TEXT').value.lower().find('todo') != -1:
+                problems.append('todo found in message')
+    if individual.get('NOTE').get('TEXT').value.lower().find('todo') != -1:
+        problems.append('todo found in bio')
+    if problems:
+        if output is not None:
+            output.write(individual.html() + '\n')
+            output.write('<h3>Problems:</h3>\n')
+            output.write('<e1>\n')
+            for problem in problems:
+                output.write('<li>' + problem + '</li>\n')
+            output.write('</e1>\n')
+        out('')
+        out('\n'.join(problems))
+        out(individual)
+        for obj in individual.getAll('OBJE'):
+            if obj.get('FORM').value == 'Message':
+                out(obj.get('TEXT').value)
 
 
 def lint(filename, options, out=print_to_console):
@@ -55,41 +94,10 @@ def lint(filename, options, out=print_to_console):
     llg = gedcom.LineageLinkedGedcom(ged)
 
     if output is not None:
-        write_report(filename, opts, output)
+        write_report_header(filename, opts, output)
 
     for individual in llg.individuals:
-        if individual.records.has_key('WWW'):
-            problems = []
-            if maxage is not None and individual.getAge() is not None and individual.getAge() > maxage:
-                problems.append(
-                    'age {0} greater than maximum age {1}'.format(individual.getAge(), maxage))
-
-            if opts.has_key('birth') and individual.get('BIRT').get('DATE') is None:
-                problems.append('missing birth date')
-            if opts.has_key('death') and individual.get('DEAT') is None:
-                problems.append('missing date of death')
-            if individual.get('SEX') is None:
-                problems.append('sex not defined')
-            for obj in individual.getAll('OBJE'):
-                if obj.get('FORM').value == 'Message':
-                    if obj.get('TEXT').value.lower().find('todo') != -1:
-                        problems.append('todo found in message')
-            if individual.get('NOTE').get('TEXT').value.lower().find('todo') != -1:
-                problems.append('todo found in bio')
-            if problems:
-                if output is not None:
-                    output.write(individual.html() + '\n')
-                    output.write('<h3>Problems:</h3>\n')
-                    output.write('<e1>\n')
-                    for problem in problems:
-                        output.write('<li>' + problem + '</li>\n')
-                    output.write('</e1>\n')
-                out('')
-                out('\n'.join(problems))
-                out(individual)
-                for obj in individual.getAll('OBJE'):
-                    if obj.get('FORM').value == 'Message':
-                        out(obj.get('TEXT').value)
+        check_individual(individual, opts, maxage, output, out)
 
     if output is not None:
         output.write('</body></html>\n')
